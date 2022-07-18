ef1e433412c11f07dcd4868a2b8c5fa71dd74f0c
neosizzle <junhanngd1115@gmail.com>
2022-07-18 17:49:33 +0800

    configure cors directus

--- launch.sh
+++ launch.sh
@@ -1,5 +1,13 @@
-sudo apt-get install docker git
-sudo git clone https://github.com/neosizzle/postgres-directus-docker-img.git
+#!/bin/bash
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+
+cd /home/ubuntu
+apt-get update -y && sudo apt-get upgrade -y
+apt-get install docker.io git curl -yqq
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+git clone https://github.com/neosizzle/postgres-directus-docker-img.git
 cd postgres-directus-docker-img
 echo "SOMEENV=VALUE" >> .env.prod
 mv .env.prod .env
